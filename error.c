63f225bba77f70150c2b18923c042976a7375c17
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-03-31 17:02:48 -0700

    Error file

--- error.c
+++ error.c
@@ -5,7 +5,7 @@
  */
 void usage_error(void)
 {
-	fprintf(stderr, "USAGE: MONTYFILE\n");
+	fprintf(stderr, "USAGE: monty file");
 	exit(EXIT_FAILURE);
 }
 /**
@@ -29,20 +29,21 @@ void push_error(FILE *fd, char *line, stack_t *stack, int line_number)
 {
 	fprintf(stderr, "L%u: usage: push integer\n", line_number);
 	fclose(fd);
+	free(line);
 	_free(stack);
 	exit(EXIT_FAILURE);
 }
 /**
- * ins_error - Error handler for unknown instructions
+ * instr_error - Error handler for unknown instructions
  * @fd: file descriptor
  * @line: buffer
  * @stack: stack or queue
  * @line_n: line number of the command
  * @value: instruction
  */
-void ins_error(file *fd, char *line, stack_t *stack, char *value, int line_n)
+void instr_error(file *fd, char *line, stack_t *stack, char *value, int line_n)
 {
-	fprint(stderr, "L%U: unlnown instruction %s\n", line_n, value);
+	fprintf(stderr, "L%U: unlnown instruction %s\n", line_n, val);
 	fclose(fd);
 	free(line);
 	_free(stack);
